41760a93461f9d07e8c034c50040f13dfe0595d8
kelvin <michael8101@126.com>
2020-07-09 10:40:16 +0800

    加了注释的代码，递归代码简单，也好理解。不明白就画递归树

--- src/main/java/com/kelvin/swap/pair/Solution.java
+++ src/main/java/com/kelvin/swap/pair/Solution.java
@@ -7,17 +7,47 @@ public class Solution {
         if ((head == null) || (head.next == null)) {
             return head;
         }
+        /*
+            1,
+            head=1,2,3,4；n=2,3,4；调用(x=3,4)
+            返回head=1,4,3,null;n=2,1,4,3,null
+            2,
+            n=4;head=3,4; 调用(x=null)；
+            返回head=3,null;n=4,3,null
+         */
         ListNode n = head.next;
         head.next = swapPairs(head.next.next);
         n.next = head;
         return n;
     }
 
+    public ListNode swapPairs1(ListNode head) {
+        if (head == null || head.next == null) return head;
+        ListNode current = head;
+        ListNode newHead = head.next;
+        while (current != null && current.next != null) {
+            //1,2,3,4,5
+            ListNode temp = current;
+            //2,3,4,5
+            current = current.next;
+            //1,3,4,5
+            temp.next = current.next;
+            //2,1,3,4,5
+            current.next = temp;
+            //3,4,5
+            current = temp.next;
+            if (current != null && current.next != null) {
+                temp.next = current.next;
+            }
+        }
+        return newHead;
+    }
+
     @Test
     public void testSwapPairs() {
         ListNode rs = null;
         ListNode head = null;
-        for (int i = 1; i <= 4; i++) {
+        for (int i = 1; i <= 5; i++) {
             ListNode next = null;
             if (i == 1) {
                 head = new ListNode(i);
@@ -29,8 +59,7 @@ public class Solution {
             }
         }
         System.out.println(rs);
-        this.swapPairs(rs);
-        System.out.println(rs);
+        System.out.println(this.swapPairs1(rs));
     }
 }
 
