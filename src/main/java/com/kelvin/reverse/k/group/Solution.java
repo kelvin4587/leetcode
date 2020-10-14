2bf40ff02ca14318e84d634ef50968ed324c0a57
kelvin <wangkun@huizrd.com>
2020-10-14 23:15:53 +0800

    2day

---
+++ src/main/java/com/kelvin/reverse/k/group/Solution.java
@@ -0,0 +1,29 @@
+package com.kelvin.reverse.k.group;
+
+import com.kelvin.ListNode;
+
+/**
+ * @author kelvin
+ * @date 2020/10/14 9:25 PM
+ */
+public class Solution {
+    public ListNode reverseKGroup(ListNode head, int k) {
+        ListNode curr = head;
+        int count = 0;
+        while (curr != null && count != k) {
+            curr = curr.next;
+            count++;
+        }
+        if (count == k) {
+            curr = reverseKGroup(curr, k);
+            while (count-- > 0) {
+                ListNode temp = head.next;
+                head.next = curr;
+                curr = head;
+                head = temp;
+            }
+            head = curr;
+        }
+        return head;
+    }
+}
