5c05da6aff28a67d058ab49717d659b63e0190c3
kelvin <michael8101@126.com>
2020-12-14 19:16:06 +0800

    20201214-3

--- src/main/java/com/kelvin/two/six/Solution.java
+++ src/main/java/com/kelvin/two/six/Solution.java
@@ -9,12 +9,21 @@ public class Solution {
             if (pre == nums[i]) {
                 step++;
             } else {
+                for (int j = i - step; j < length; j++) {
+                    int temp = nums[j];
+                    nums[j] = nums[j + step];
+                    nums[j + step] = temp;
+                }
                 pre = nums[i - 1];
+                length = length - step;
+                step = 0;
             }
         }
+        return length;
     }
 
     public static void main(String[] args) {
         int[] nums = {0, 0, 1, 1, 1, 2, 2, 3, 3, 4};
+        System.out.println(new Solution().removeDuplicates(nums));
     }
 }
