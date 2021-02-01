fb2ebb337397e22c9859d58776f80e0367e4b82e
kelvin <michael8101@126.com>
2021-02-01 17:10:46 +0800

    two numbers add

---
+++ src/main/java/com/kelvin/Solution2_1.java
@@ -0,0 +1,11 @@
+package com.kelvin;
+
+
+
+public class Solution2_1 {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        int val1=l1!=null?l1.val:0;
+        int val2=l2!=null?l2.val:0;
+        return null;
+    }
+}
--- src/main/java/com/kelvin/one/eight/nine/second/Solution.java
+++ src/main/java/com/kelvin/one/eight/nine/second/Solution.java
@@ -6,17 +6,20 @@ public class Solution {
         for (int i = 0; i < nums.length; i++) {
             int temp = nums[i];
             offset = i + offset;
-            if(offset<nums.length) {
+            if (offset < nums.length) {
                 nums[i] = nums[offset];
-                nums[offset] = temp;
-            }else{
-                nums[i] = nums[offset-nums.length];
-                nums[offset-nums.length] = temp;
+                i = offset;
+            } else {
+                nums[i] = nums[offset - nums.length];
             }
         }
     }
 
     public static void main(String[] args) {
         int[] nums = {1, 2, 3, 4, 5, 6, 7};
+        new Solution().rotate(nums, 3);
+        for (int i = 0; i < nums.length; i++) {
+            System.out.println(nums[i]);
+        }
     }
 }
