62447945bfad584fc92f6e8c6d1cc5cd4795f061
kelvin <michael8101@126.com>
2021-02-04 13:16:16 +0800

    自己写的不知道能不能提交啊。哈哈。

--- src/main/java/com/kelvin/Solution26_1.java
+++ src/main/java/com/kelvin/Solution26_1.java
@@ -4,14 +4,27 @@ import java.util.Arrays;
 
 public class Solution26_1 {
     public static int removeDuplicates(int[] nums) {
-        int i = 0, j = 0;
+        int i = 0, j = 1, k = 0;
         int n = nums.length;
         while (i < n) {
             if (nums[i] == nums[j]) {
                 j++;
+            } else {
+                n=move(nums, i, j, n);
+                i++;
             }
         }
+        return n;
+    }
 
+    public static int move(int[] nums, int i, int j, int n) {
+        int step = j - i - 1;
+        n = n - step;
+        for (int k = i + 1; k < n; k++) {
+            int temp = nums[k];
+            nums[k] = nums[k + step];
+            nums[k + step] = temp;
+        }
         return n;
     }
 
