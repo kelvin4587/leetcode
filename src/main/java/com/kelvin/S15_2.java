fb1c7c69bbc072ef1dcdfa06f86dc7d81e304f5f
Kelvin Wang <michael8101@126.com>
2021-10-19 16:52:25 +0800

    用map解决重复问题

--- src/main/java/com/kelvin/S15_2.java
+++ src/main/java/com/kelvin/S15_2.java
@@ -1,27 +1,34 @@
 package com.kelvin;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class S15_2 {
     public static List<List<Integer>> threeSum(int[] nums) {
         int i = 0;
         int j = nums.length - 1;
         List<List<Integer>> ans = new ArrayList<>();
+        Set<Integer> minusSumKey= new HashSet<>();
         while (i < j) {
-            int sum = nums[i] + nums[j];
-            for(int k=i+1;k<j;k++) {
-                if (sum == -nums[k]) {
-                    List<Integer> an = new ArrayList<>();
-                    an.add(nums[i]);
-                    an.add(nums[j]);
-                    an.add(nums[k]);
-                    ans.add(an);
+            int oj=j;
+            while (i < j) {
+                int sum = nums[i] + nums[j];
+                for (int k = i + 1; k < j; k++) {
+                    if (sum == -nums[k]) {
+                        if(!minusSumKey.contains(-nums[k])) {
+                            List<Integer> an = new ArrayList<>();
+                            an.add(nums[i]);
+                            an.add(nums[j]);
+                            an.add(nums[k]);
+                            ans.add(an);
+                            minusSumKey.add(-nums[k]);
+                            minusSumKey.add(-(nums[k]+nums[j]));
+                        }
+                    }
                 }
+                j--;
             }
+            j=oj;
             i++;
-            j--;
         }
         return ans;
     }
