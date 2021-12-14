d7f57a3bd2106622973bff5abf930bbe70d3c4d0
kelvin <wangkun@huizrd.com>
2021-12-15 06:00:57 +0800

    two sum iteration

--- src/main/java/com/kelvin/Solution1_2.java
+++ src/main/java/com/kelvin/Solution1_2.java
@@ -4,7 +4,14 @@ package com.kelvin;
 public class Solution1_2 {
     public int[] twoSum(int[] nums, int target) {
         int[] result = new int[2];
-
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    result[0] = nums[i];
+                    result[1] = nums[j];
+                }
+            }
+        }
         return result;
     }
 
