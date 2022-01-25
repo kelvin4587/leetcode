bb03c2fe5022a97957f4c329471c366ea12b05c9
kelvin <michael8101@126.com>
2022-01-25 15:38:15 +0800

    测试环

--- src/main/java/com/kelvin/S142_1.java
+++ src/main/java/com/kelvin/S142_1.java
@@ -4,9 +4,9 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class S142_1 {
-    public ListNode detectCycle(ListNode head) {
-        ListNode pos = head;
-        Set<ListNode> visited = new HashSet<ListNode>();
+    public ListNodeCycle detectCycle(ListNodeCycle head) {
+        ListNodeCycle pos = head;
+        Set<ListNodeCycle> visited = new HashSet<ListNodeCycle>();
         while (pos != null) {
             if (visited.contains(pos)) {
                 return pos;
@@ -17,8 +17,39 @@ public class S142_1 {
         }
         return null;
     }
-
+    public ListNodeCycle detectCycle2(ListNodeCycle head) {
+        if(head==null){
+            return null;
+        }
+        ListNodeCycle slow = head,fast=head;
+        while (fast != null) {
+            slow = slow.next;
+            if(fast.next!=null){
+                fast=fast.next.next;
+            }else{
+                return null;
+            }
+            if(fast==slow){
+                ListNodeCycle ptr = head;
+                while(ptr!=slow){
+                    ptr=ptr.next;
+                    slow=slow.next;
+                }
+                return ptr;
+            }
+        }
+        return null;
+    }
     public static void main(String[] args) {
+        ListNodeCycle test=new ListNodeCycle(3);
+        ListNodeCycle n2=new ListNodeCycle(2);
+        test.next = n2;
+        test.next.next = new ListNodeCycle(0);
+        test.next.next.next = new ListNodeCycle(-4);
+        test.next.next.next.next = n2;
+        S142_1 s142_1 = new S142_1();
+        ListNodeCycle node = s142_1.detectCycle2(test);
+        System.out.println(node);
 
     }
 }
