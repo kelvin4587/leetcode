023094d13aaf9d4ba612fad019dad31cde926c8b
kelvin <michael8101@126.com>
2022-01-27 18:29:04 +0800

    这简单拜托你能完成

---
+++ src/main/java/com/kelvin/S189_1.java
@@ -0,0 +1,15 @@
+package com.kelvin;
+
+import java.util.Arrays;
+
+public class S189_1 {
+
+    public void rotate(int[] nums, int k) {
+        for(int i=0;i<nums.length;i++){ }
+    }
+
+    public static void main(String[] args) {
+        int[] data = {1, 2, 3, 4, 5, 6, 7};
+        System.out.println(Arrays.toString(data));
+    }
+}
--- src/main/java/com/kelvin/S26_1.java
+++ src/main/java/com/kelvin/S26_1.java
@@ -2,29 +2,35 @@ package com.kelvin;
 
 public class S26_1 {
     public int removeDuplicates(int[] nums) {
-        for (int i = 0; i < nums.length; i++) {
+        int length = nums.length;
+        for (int i = 0; i < length; i++) {
             int j = i + 1;
-            while (j < nums.length) {
+            while (j < length) {
                 if (nums[i] == nums[j]) {
                     j++;
+                }else{
+                    length=this.moveArray(nums,i,j);length=this.moveArray(nums,i,j);
+                    break;
                 }
             }
-
-
         }
-        return 0;
+        return length;
     }
 
     public int moveArray(int[] nums, int start, int end) {
-        int steps = end - start + 1;
+        int steps = end - start - 1 ;
         int length = nums.length - steps;
         for (int i = start + 1; i < length; i++) {
-
+            nums[i] = nums[i+steps];
         }
         return length;
     }
 
     public static void main(String[] args) {
-
+        int[] test = {1,1,2};
+        S26_1 s26_1 = new S26_1();
+        int i = s26_1.removeDuplicates(test);
+        System.out.println(test);
+        System.out.println(i);
     }
 }
