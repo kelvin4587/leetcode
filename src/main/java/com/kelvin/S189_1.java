d6bdcea2117660eb1f7bb0325beb8c5f4027bbb2
kelvin <michael8101@126.com>
2022-01-28 11:02:26 +0800

    晚了，但我已经尽力。

--- src/main/java/com/kelvin/S189_1.java
+++ src/main/java/com/kelvin/S189_1.java
@@ -5,11 +5,19 @@ import java.util.Arrays;
 public class S189_1 {
 
     public void rotate(int[] nums, int k) {
-        for(int i=0;i<nums.length;i++){ }
+        for (int j = 1; j <= k; j++) {
+            int temp = nums[nums.length - 1];
+            for (int i = nums.length - 1; i > 0; i--) {
+                nums[i] = nums[i - 1];
+            }
+            nums[0] = temp;
+        }
     }
 
     public static void main(String[] args) {
         int[] data = {1, 2, 3, 4, 5, 6, 7};
+        S189_1 s189_1 = new S189_1();
+        s189_1.rotate(data,3);
         System.out.println(Arrays.toString(data));
     }
 }
