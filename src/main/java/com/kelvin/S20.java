d9748f1eb08fe99ed4190fe9eecd920842a6c533
kelvin <michael8101@126.com>
2022-03-09 15:40:57 +0800

    昨天没有完成，今天补上

--- src/main/java/com/kelvin/S20.java
+++ src/main/java/com/kelvin/S20.java
@@ -1,6 +1,8 @@
 package com.kelvin;
 
+import java.util.Deque;
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.Map;
 
 public class S20 {
@@ -14,7 +16,32 @@ public class S20 {
             put('}', '{');
             put(')', '(');
         }};
-        return true;
+        Deque<Character> stack = new LinkedList<>();
+        for (int i = 0; i < length; i++) {
+            Character c = s.charAt(i);
+            if (map.containsKey(c)) {
+                if (stack.isEmpty() || !map.get(c).equals(stack.peek())) {
+                    return false;
+                }
+                stack.pop();
+            }else{
+                stack.push(c);
+            }
+        }
+        return stack.isEmpty();
     }
 
+    public static void main(String[] args) {
+        String s1 = "()";
+        String s2 = "()[]{}";
+        String s3 = "([)]";
+        String s4 = "(]";
+        String s5 = "{[]}";
+        S20 s = new S20();
+        System.out.printf("%s:%b",s1,s.isValid(s1));
+        System.out.printf("%s:%b",s2,s.isValid(s2));
+        System.out.printf("%s:%b",s3,s.isValid(s3));
+        System.out.printf("%s:%b",s4,s.isValid(s4));
+        System.out.printf("%s:%b",s5,s.isValid(s5));
+    }
 }
