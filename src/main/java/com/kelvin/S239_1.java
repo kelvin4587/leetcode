60aead954baffee8b6aa7c3899d8b041a1e743a8
Kelvin Wang <michael8101@126.com>
2022-08-30 16:12:12 +0800

    滑动窗口

---
+++ src/main/java/com/kelvin/S239_1.java
@@ -0,0 +1,16 @@
+package com.kelvin;
+
+public class S239_1 {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+
+        for(int i=0;i<nums.length;i++){
+
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] nums ={1,3,-1,-3,5,3,6,7};
+        int k=3;
+        new S239_1().maxSlidingWindow(nums,k);
+    }
+}
