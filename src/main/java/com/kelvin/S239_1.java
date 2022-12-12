955e780d814fb9cd5ec80e3e423a0ce8e63bea8c
Kelvin Wang <michael8101@126.com>
2022-12-12 16:40:36 +0800

    滑动窗口

--- src/main/java/com/kelvin/S239_1.java
+++ src/main/java/com/kelvin/S239_1.java
@@ -1,16 +1,32 @@
 package com.kelvin;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class S239_1 {
     public int[] maxSlidingWindow(int[] nums, int k) {
-
-        for(int i=0;i<nums.length;i++){
-
+        int[] result = new int[nums.length-k+1];
+        int start = 0;
+        for (int i = 0; i < result.length; i++) {
+            int end = start + k - 1;
+            if (end >= nums.length) {
+                break;
+            }
+            int max = Integer.MIN_VALUE;
+            for (int j = start; j <= end; j++) {
+                max = Integer.max(nums[j],max);
+            }
+            result[i] = max;
+            start++;
         }
+        return result;
     }
 
     public static void main(String[] args) {
-        int[] nums ={1,3,-1,-3,5,3,6,7};
-        int k=3;
-        new S239_1().maxSlidingWindow(nums,k);
+        int[] nums = {1, 3, -1, -3, 5, 3, 6, 7};
+        int k = 3;
+        int[] result = new S239_1().maxSlidingWindow(nums, k);
+        System.out.println(Arrays.toString(result));
     }
 }
---
+++ src/main/java/com/kelvin/S239_1_1.java
@@ -0,0 +1,87 @@
+package com.kelvin;
+
+import java.util.*;
+
+public class S239_1_1 {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        int n = nums.length;
+        PriorityQueue<int[]> pq = new PriorityQueue<int[]>(new Comparator<int[]>() {
+            @Override
+            public int compare(int[] pair1, int[] pair2) {
+                return pair1[0] != pair2[0] ? pair2[0] - pair1[0] : pair2[1] - pair1[1];
+            }
+        });
+        for (int i = 0; i < k; ++i) {
+            pq.offer(new int[]{nums[i], i});
+        }
+        int[] ans = new int[n - k + 1];
+        ans[0] = pq.peek()[0];
+        for (int i = k; i < n; ++i) {
+            pq.offer(new int[]{nums[i], i});
+            while (pq.peek()[1] <= i - k) {
+                pq.poll();
+            }
+            ans[i-k+1] = pq.peek()[0];
+        }
+        return ans;
+    }
+
+
+    public int[] maxSlidingWindow2(int[] nums, int k) {
+        int n = nums.length;
+        Deque<Integer> deque = new LinkedList<Integer>();
+        for (int i = 0; i < k; ++i) {
+            while (!deque.isEmpty() && nums[i] >= nums[deque.peekLast()]) {
+                deque.pollLast();
+            }
+            deque.offerLast(i);
+        }
+
+        int[] ans = new int[n - k + 1];
+        ans[0] = nums[deque.peekFirst()];
+        for (int i = k; i < n; ++i) {
+            while (!deque.isEmpty() && nums[i] >= nums[deque.peekLast()]) {
+                deque.pollLast();
+            }
+            deque.offerLast(i);
+            while (deque.peekFirst() <= i - k) {
+                deque.pollFirst();
+            }
+            ans[i - k + 1] = nums[deque.peekFirst()];
+        }
+        return ans;
+    }
+    public int[] maxSlidingWindow3(int[] nums, int k) {
+        int n = nums.length;
+        int[] prefixMax = new int[n];
+        int[] suffixMax = new int[n];
+        for (int i = 0; i < n; ++i) {
+            if (i % k == 0) {
+                prefixMax[i] = nums[i];
+            }
+            else {
+                prefixMax[i] = Math.max(prefixMax[i - 1], nums[i]);
+            }
+        }
+        for (int i = n - 1; i >= 0; --i) {
+            if (i == n - 1 || (i + 1) % k == 0) {
+                suffixMax[i] = nums[i];
+            } else {
+                suffixMax[i] = Math.max(suffixMax[i + 1], nums[i]);
+            }
+        }
+
+        int[] ans = new int[n - k + 1];
+        for (int i = 0; i <= n - k; ++i) {
+            ans[i] = Math.max(suffixMax[i], prefixMax[i + k - 1]);
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {3, 1, -1, -3, 5, 3, 6, 7};
+        int k = 3;
+        int[] result = new S239_1_1().maxSlidingWindow(nums, k);
+        System.out.println(Arrays.toString(result));
+    }
+}
