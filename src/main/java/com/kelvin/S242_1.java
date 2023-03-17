523dece320c7806628bf67aa0e42cda7bd1d62c3
Kelvin Wang <michael8101@126.com>
2023-03-17 15:02:09 +0800

    回家再搞吧

---
+++ src/main/java/com/kelvin/S242_1.java
@@ -0,0 +1,15 @@
+package com.kelvin;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class S242_1 {
+    public boolean isAnagram(String s, String t) {
+        return  false;
+    }
+
+    private Map<String,Integer> convertToMap(String s){
+        Map<String,Integer> result = new HashMap<>();
+        return result;
+    }
+}
