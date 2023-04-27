3e5d44c3ca40734cb9925a33b86fd9c07fed07de
Kelvin Wang <michael8101@126.com>
2023-04-27 15:38:36 +0800

    我的第一个五遍题目，我应该掌握了，可喜可贺

--- src/main/java/com/kelvin/S1_5.java
+++ src/main/java/com/kelvin/S1_5.java
@@ -1,8 +1,24 @@
 package com.kelvin;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class S1_5 {
     public int[] twoSum(int[] nums, int target) {
-        return null;
+        int[] result = new int[2];
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int num1 = nums[i];
+            int num2 = target - num1;
+            if (map.containsKey(num2)) {
+                result[0] = map.get(num2);
+                result[1] = i;
+            } else {
+                map.put(num1, i);
+            }
+
+        }
+        return result;
     }
 
     public static void main(String[] args) {
@@ -12,6 +28,6 @@ public class S1_5 {
         int[] nums2 = {3, 2, 4};
         System.out.printf("[%d,%d]", s.twoSum(nums2, 6)[0], s.twoSum(nums2, 6)[1]);
         int[] nums3 = {3, 3};
-        System.out.printf("[%d,%d]", s.twoSum(nums3, 6)[0], s.twoSum(nums3, 6)[1]);
+        System.out.printf("[%d,%d]", s.twoSum(nums3, 7)[0], s.twoSum(nums3, 7)[1]);
     }
 }
