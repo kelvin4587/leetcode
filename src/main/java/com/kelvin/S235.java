96c77eb8e8987e3295d59f0d6dad3c4e505f2e63
Kelvin Wang <michael8101@126.com>
2023-11-01 16:51:07 +0800

    回家完成

--- src/main/java/com/kelvin/S235.java
+++ src/main/java/com/kelvin/S235.java
@@ -1,7 +1,26 @@
 package com.kelvin;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class S235 {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+
         return null;
+    }
+    public List<TreeNode> getPath(TreeNode root, TreeNode target){
+        List<TreeNode> path = new ArrayList<>();
+        TreeNode node = root;
+        while(node.val!=target.val){
+            if(node.val<target.val){
+                path.add(node);
+                node=node.right;
+            }else if(node.val>target.val){
+                path.add(node);
+                node=node.left;
+            }
+        }
+        path.add(node);
+        return path;
     }
 }
