67f0ad3de4d85e90b6fe7ed97af082335aeef2ff
Kelvin Wang <michael8101@126.com>
2023-11-03 10:56:12 +0800

    卡住了，突破自己不要待着舒适区

--- src/main/java/com/kelvin/S235.java
+++ src/main/java/com/kelvin/S235.java
@@ -9,6 +9,31 @@ public class S235 {
         return null;
     }
 
+    public static void main(String[] args) {
+        Integer[] data = {6, 2, 8, 0, 4, 7, 9, null, null, 3, 5};
+        TreeNode root = new TreeNode();
+        root.val = data[0];
+        int count = 0;
+        int length = power(2, count);
+        int i = 1;
+        while (i < length) {
+
+        }
+
+    }
+
+    public static int power(int base, int times) {
+        int result = base;
+        for (int i = 0; i < times; i++) {
+            if (i == 0) {
+                result = 1;
+            } else {
+                result = result * base;
+            }
+        }
+        return result;
+    }
+
     public List<TreeNode> getPath(TreeNode root, TreeNode target) {
         List<TreeNode> path = new ArrayList<>();
         TreeNode node = root;
